101536b828c816e4447ac0648a77ddf3be5029f4
StanleyTYLau <stanley.tyl@gmail.com>
2019-04-01 13:03:51 -0400

    custom packages

--- 03_packages/main.go
+++ 03_packages/main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"fmt"
 	"math"
+
+	"StanleyTYLau/go_basics/03_packages/strutil"
 )
 
 func main() {
 	fmt.Println(math.Floor(2.9))
+	fmt.Println(math.Sqrt(81))
+	fmt.Println(strutil.Reverse("Hellooooo"))
 }
